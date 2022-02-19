31a600e93c102a5bf170d1a55a5fbc5ffd0b5874
Zeyad <zyabdelwahab1@sheffield.ac.uk>
2022-02-19 18:02:32 +0200

    Add CustomerRequestDtos.

---
+++ OrderAPI.Dtos/Dtos/CustomerDtos/CustomerResponseDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderAPI.Dtos.CustomerDtos
+{
+    public class CustomerResponseDto
+    {
+        public string UserName { get; set; }
+        public string JwtToken { get; set; }
+    }
+}
