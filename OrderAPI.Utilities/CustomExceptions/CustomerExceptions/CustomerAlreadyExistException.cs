976f273b72cb44ffefbb56cecea99c53e3f8ae3c
Zeyad <zyabdelwahab1@sheffield.ac.uk>
2022-02-19 18:05:14 +0200

    Add CustomExceptions for Customers.

---
+++ OrderAPI.Utilities/CustomExceptions/CustomerExceptions/CustomerAlreadyExistException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderAPI.Utilities.CustomExceptions
+{
+    public class CustomerAlreadyExistException: Exception
+    {
+        public CustomerAlreadyExistException(string message)
+      : base(message)
+        {
+        }
+    }
+}
---
+++ OrderAPI.Utilities/CustomExceptions/DatabaseSaveException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderAPI.Utilities.CustomExceptions
+{
+    public class DatabaseSaveException: Exception
+    {
+        public DatabaseSaveException(string message)
+        : base(message)
+        {
+        }
+    }
+}
