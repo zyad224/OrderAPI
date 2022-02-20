26aac007f8a92158f2f69117ff8a1fde4fb82b2a
Zeyad <zyabdelwahab1@sheffield.ac.uk>
2022-02-20 17:35:45 +0200

    Add IJwtService.

---
+++ OrderAPI.Services/IJwtService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderAPI.Services
+{
+    public interface IJwtService
+    {
+        public string GenerateJWT();
+    }
+}
---
+++ OrderAPI.Services/JwtService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace OrderAPI.Services
+{
+    public class JwtService : IJwtService
+    {
+        private readonly IConfiguration _configuration;
+
+
+        public JwtService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public string GenerateJWT()
+        {
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_configuration["Jwt:Issuer"],
+              _configuration["Jwt:Audience"],
+              null,
+              expires: DateTime.Now.AddMinutes(60),
+              signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
