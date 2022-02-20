94ec9f5decbc2fa3baea6581122e10ac2f85598d
Zeyad <zyabdelwahab1@sheffield.ac.uk>
2022-02-20 02:41:34 +0200

    Add OrderDtos.

---
+++ OrderAPI.Dtos/Dtos/OrderDtos/OrderRequestDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace OrderAPI.Dtos.Dtos.OrderDtos
+{
+    public class OrderRequestDto
+    {
+        [Required(ErrorMessage = "OrderId is Required"), MaxLength(30)]
+        public string OrderId { get; set; }
+        [Required(ErrorMessage = "CustomerId is Required"), MaxLength(30)]
+        public string CustomerId { get; set; }
+        [Required(ErrorMessage = "ProductTypesQuantities is Required")]
+        public List<ProductTypeQuantityDto> ProductTypesQuantities { get; set; } = new List<ProductTypeQuantityDto>();
+
+    }
+}
---
+++ OrderAPI.Dtos/Dtos/OrderDtos/ProductTypeDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderAPI.Dtos.Dtos.OrderDtos
+{
+    public enum ProductTypeDto
+    {
+        NoProductType = 0,
+        photoBook = 1,
+        canvas = 2,
+        cards = 3,
+        mug = 4
+    }
+}
