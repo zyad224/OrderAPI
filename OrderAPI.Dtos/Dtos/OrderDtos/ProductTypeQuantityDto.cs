72ed92b0f0b42060b8cf226bd133bb8458b5b41a
Zeyad <zyabdelwahab1@sheffield.ac.uk>
2022-02-22 17:50:05 +0200

    Trigger Azure CI/CD from master.

--- OrderAPI.Dtos/Dtos/OrderDtos/ProductTypeQuantityDto.cs
+++ OrderAPI.Dtos/Dtos/OrderDtos/ProductTypeQuantityDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace OrderAPI.Dtos.Dtos.OrderDtos
@@ -7,6 +8,7 @@ namespace OrderAPI.Dtos.Dtos.OrderDtos
     public class ProductTypeQuantityDto
     {
         public ProductTypeDto ProductType { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Please enter postive value")]
         public int Quantity { get; set; }
 
     }
